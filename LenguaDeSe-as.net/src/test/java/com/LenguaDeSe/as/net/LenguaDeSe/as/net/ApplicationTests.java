edfc671105ad973eb3056990879b38981eea296b
Sam <sam@iMac-de-Sam.local>
2020-10-21 17:19:35 -0500

    proyecto inicializado

---
+++ LenguaDeSe-as.net/src/test/java/com/LenguaDeSe/as/net/LenguaDeSe/as/net/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LenguaDeSe.as.net.LenguaDeSe.as.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
