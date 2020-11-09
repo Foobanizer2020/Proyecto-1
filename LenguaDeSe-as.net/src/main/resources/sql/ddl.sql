d7e81527e04319de91043bd22394c302626e3f39
Raúl González <gonzalv@ciencias.unam.mx>
2020-11-09 13:23:34 -0600

    Agrega al usuario registrado como admin (esto se debe cambias a usuario normal)

--- LenguaDeSe-as.net/src/main/resources/sql/ddl.sql
+++ LenguaDeSe-as.net/src/main/resources/sql/ddl.sql
@@ -3,3 +3,6 @@ insert into tipo_usuario (descripcion, nombre_tipo_usuario) values
 	("Rol para editor", "EDITOR"),
 	("Rol para usuario", "USUARIO");
 	
+insert into paises (abreviatura, pais) values ("MX", "Mexico"), ("US", "Estados Unidos");
+
+insert into estado (nombre, pais) values ("Guanajuato", 1), ("Queretaro", 1), ("New York", 2);
