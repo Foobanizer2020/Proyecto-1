53d1cbbfe61b4fa9de6447e56f51f83d9b6cecf2
Raúl González <gonzalv@ciencias.unam.mx>
2020-11-09 20:54:05 -0600

    Configura swagger3 con el backend

--- LenguaDeSe-as.net/src/main/java/com/LenguaDeSe/as/net/LenguaDeSe/as/net/config/WebSecurityConfig.java
+++ LenguaDeSe-as.net/src/main/java/com/LenguaDeSe/as/net/LenguaDeSe/as/net/config/WebSecurityConfig.java
@@ -40,7 +40,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				"/api/autenticacion/login/**", 
 				"/api/autenticacion/registro/**",
 				"/pais/paises",
-				"/api/estado/pais/**"
+				"/api/estado/pais/**",
+				"/v3/api-docs/**",
+				"/swagger-ui/**",
+				"/swagger-ui*"
 		).permitAll()
 		.antMatchers("/api/estado/**").hasAuthority("ADMINISTRADOR").anyRequest().authenticated().and().csrf()
 		.disable().exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint()).and()
