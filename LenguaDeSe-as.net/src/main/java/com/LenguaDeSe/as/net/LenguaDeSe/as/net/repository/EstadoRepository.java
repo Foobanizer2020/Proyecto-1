ab76c564223e705bf501e88320ca4856b1461ee0
Raúl González <gonzalv@ciencias.unam.mx>
2020-11-25 20:35:44 -0600

    Posible corrección?

--- LenguaDeSe-as.net/src/main/java/com/LenguaDeSe/as/net/LenguaDeSe/as/net/repository/EstadoRepository.java
+++ LenguaDeSe-as.net/src/main/java/com/LenguaDeSe/as/net/LenguaDeSe/as/net/repository/EstadoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.LenguaDeSe.as.net.LenguaDeSe.as.net.model.Estado;
@@ -11,5 +12,5 @@ import com.LenguaDeSe.as.net.LenguaDeSe.as.net.model.Estado;
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 	@Query(value="SELECT * FROM estado WHERE pais = :paisId", nativeQuery=true)
-	List<Estado> findByPaisId(Integer paisId);
+	List<Estado> findByPaisId(@Param("paisId") Integer paisId);
 }
